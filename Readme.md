f687df886d3795d308a81eb3340ac047ab0d35ca
Shane McDonagh <G00371430@gmit.ie>
2022-04-11 16:27:47 +0100

    Work-in-progress Twilio application

---
+++ Readme.md
@@ -0,0 +1,24 @@
+Explanation of application
+--------------------------
+Requirements
+------------
+Twilio Account and Phone Number
+ngrok - Cross-platform application to expose local server post to the internet (moreso for demo purposes)
+Google Cloud Account (which has Google Speech-To-Text API enabled, and a service account generated with an access key)
+Google Cloud SDK
+
+Dependencies
+------------
+Express
+Websocket
+Google Cloud Speech-To-Text
+dot.env
+
+1. When a call comes through to our twilio number it make a HTTP POST request to a ngrok url, tunneling to the localhost
+2. The server then sends a TwiML response to the request, which in turn begins our audio stream
+3. Through the use of Websockets, we can handle the audio stream which is passed to the server by Twilio.
+4. This stream is then passed to the Google Speech-To-Text APi which is then displayed to the console and the browser
+
+Problems
+--------
+We can currently retrieve both streams, however it is extremely slow and not as reliable as we hoped
---
+++ index.js
@@ -0,0 +1,90 @@
+const WebSocket = require("ws");
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const path = require("path");
+
+// Used to retrieve the necessary credentials for API calls to Google Cloud
+require("dotenv").config();
+
+//Include Google Speech to Text
+const speech = require("@google-cloud/speech");
+const client = new speech.SpeechClient();
+
+//Configure Transcription Request for Google Cloud
+const request = {
+  config: {
+    encoding: "MULAW",
+    sampleRateHertz: 8000,
+    languageCode: "en-GB",
+  },
+  interimResults: false,
+};
+
+wss.on("connection", function connection(ws) {
+  console.log("New Connection Initiated");
+
+  let recognizeStream = null;
+
+  ws.on("message", function incoming(message) {
+    const msg = JSON.parse(message);
+    switch (msg.event) {
+      case "connected":
+        console.log(`A new call has connected.`);
+        break;
+      case "start":
+        console.log(`Starting Media Stream ${msg.streamSid}`);
+        // Create Stream to the Google Speech to Text API
+        recognizeStream = client
+          .streamingRecognize(request)
+          .on("error", console.error)
+          .on("data", (data) => {
+            // Once transcription is passed in, display in console
+            console.log(data.results[0].alternatives[0].transcript);
+            wss.clients.forEach((client) => {
+              if (client.readyState === WebSocket.OPEN) {
+                client.send(
+                  JSON.stringify({
+                    event: "interim-transcription",
+                    text: data.results[0].alternatives[0].transcript,
+                  })
+                );
+              }
+            });
+          });
+        break;
+      case "media":
+        // Write Media Packets to the recognize stream
+        recognizeStream.write(msg.media.payload);
+        break;
+      case "stop":
+        console.log(`Call Has Ended`);
+        recognizeStream.destroy();
+        break;
+    }
+  });
+});
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/index.html")));
+
+app.post("/", (req, res) => {
+  res.set("Content-Type", "text/xml");
+
+  // TwiML response when POST request has been made through the phone call
+  res.send(`
+    <Response>
+      <Start>
+        <Stream url="wss://${req.headers.host}/"/>
+      </Start>
+      <Dial>+353 87 904 4178</Dial>
+      <Say>This is a demo application of Twilio Media Streams in working</Say>
+    </Response>
+  `);
+});
+
+console.log("Listening on Port 8080");
+server.listen(8080);
